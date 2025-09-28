5aa538c0a4fb8af622b2ae73843c4dd781be77ab
burak behlül <burakyabgu@gmail.com>
2025-09-28 13:33:58 +0300

    build: Set up electron

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div className='App'>
+     MERHABA ELECTRON + REACT!
+	 <div className="bg-rose-600">he</div>
+    </div>
+  )
+}
+
+export default App
