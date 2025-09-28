c3305988630ded1204c041ed34454a6d84f5edf1
burak behlül <burakyabgu@gmail.com>
2025-09-28 14:22:53 +0300

    feat: Add container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
+import Container from './Container'
 import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
   return (
-    <div className='App'>
-     MERHABA ELECTRON + REACT!
-	 <div className="bg-rose-600">he</div>
-    </div>
+    <>
+		<Container />
+    </>
   )
 }
 
---
+++ src/Container.tsx
@@ -0,0 +1,17 @@
+
+function Container() {
+  return (
+    <div className="flex h-screen">
+	  <div className="basis-1/3 bg-blue-500 p-4">
+		%30 Kutu
+	  </div>
+
+	  <div className="basis-2/3 bg-green-500 p-4">
+		%70 Kutu
+	  </div>
+	</div>
+
+  )
+}
+
+export default Container
