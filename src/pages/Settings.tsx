993ec548c83a94bc6a98d8a577d01b91f8f8aed1
burak behlül <burakyabgu@gmail.com>
2025-11-07 18:15:01 +0300

    feat: Add language template schema

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,9 +1,26 @@
+import { useTranslation } from "react-i18next";
+
 function Settings() {
+  const { t, i18n } = useTranslation();
+
+  const handleChange = (e) => {
+    const lang = e.target.value;
+    i18n.changeLanguage(lang);
+  };
 
   return (
-    <>
-		AYARLAR
-    </>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <label htmlFor="lang">{t("select_language")}</label>
+      <select
+        id="lang"
+        value={i18n.language}
+        onChange={handleChange}
+        style={{ marginLeft: "10px" }}
+      >
+        <option value="en">English</option>
+        <option value="tr">Türkçe</option>
+      </select>
+    </div>
   );
 }
 
